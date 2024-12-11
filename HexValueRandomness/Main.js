30fa2fef9159c5540cfe2415821ee5db4f81ddb3
Ryan JG <ryangregoire286@gmail.com>
2024-12-11 17:53:18 -0500

    Added And Learned About Interfaces And Classes OOP Programming TypeScript

--- HexValueRandomness/Main.js
+++ HexValueRandomness/Main.js
@@ -1,8 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Main = void 0;
 var readline = require("readline-sync");
 var numOfItems = parseInt(readline.question("Enter Number of Items: "));
+var Main = /** @class */ (function () {
+    function Main() {
+    }
+    return Main;
+}());
+exports.Main = Main;
 for (var i = 0; i < numOfItems; i++) {
     var randomNum = Math.floor(Math.random() * 0xffffff).toString(16);
-    console.log(randomNum);
+    var randomRValue = Math.floor(Math.random() * 255);
+    var randomGValue = Math.floor(Math.random() * 155);
+    var randomBValue = Math.floor(Math.random() * 125);
+    var red = (new Main().r = randomRValue);
+    var green = (new Main().r = randomGValue);
+    var blue = (new Main().r = randomBValue);
+    console.log("Red Color: ".concat(red));
+    console.log("Green Color: ".concat(green));
+    console.log("Blue Color: ".concat(blue));
+    console.log("Random Hex Color Code: ".concat(randomNum));
 }
--- HexValueRandomness/Main.ts
+++ HexValueRandomness/Main.ts
@@ -2,8 +2,28 @@ import * as readline from "readline-sync";
 
 const numOfItems = parseInt(readline.question("Enter Number of Items: "));
 
+export interface NumsRange {
+  r: number;
+}
+
+export class Main implements NumsRange {
+  r: number;
+}
+
 for (let i = 0; i < numOfItems; i++) {
   const randomNum = Math.floor(Math.random() * 0xffffff).toString(16);
 
-  console.log(randomNum);
+  const randomRValue = Math.floor(Math.random() * 255);
+  const randomGValue = Math.floor(Math.random() * 155);
+  const randomBValue = Math.floor(Math.random() * 125);
+
+  const red = (new Main().r = randomRValue);
+  const green = (new Main().r = randomGValue);
+  const blue = (new Main().r = randomBValue);
+
+  console.log(`Red Color: ${red}`);
+  console.log(`Green Color: ${green}`);
+  console.log(`Blue Color: ${blue}`);
+
+  console.log(`Random Hex Color Code: ${randomNum}`);
 }
